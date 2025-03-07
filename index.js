91125422712a1439006f6b7e02a4c8c3af84f0e6
komalsoniii <komalsonii351@gmail.com>
2025-03-07 23:46:08 +0530

    DB port added

--- index.js
+++ index.js
@@ -14,65 +14,65 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("✅ Server is running!");
-});
+// app.get("/", (req, res) => {
+//   res.send("✅ Server is running!");
+// });
 
 //====================== Get School list api ====================================
-// app.get("/listSchools", async (req, res) => {
-//   try {
-//     const { latitude, longitude } = req.body;
+app.get("/listSchools", async (req, res) => {
+  try {
+    const { latitude, longitude } = req.body;
 
-//     if (!latitude || !longitude) {
-//       return res
-//         .status(400)
-//         .json({ message: "Latitude and longitude are required" });
-//     }
+    if (!latitude || !longitude) {
+      return res
+        .status(400)
+        .json({ message: "Latitude and longitude are required" });
+    }
 
-//     const [schools] = await conn.execute("SELECT * FROM schools");
-//     const userLat = parseFloat(latitude);
-//     const userLng = parseFloat(longitude);
+    const [schools] = await conn.execute("SELECT * FROM schools");
+    const userLat = parseFloat(latitude);
+    const userLng = parseFloat(longitude);
 
-//     const sortedSchools = schools
-//       .map((school) => ({
-//         ...school,
-//         distance: calculateDistance(
-//           userLat,
-//           userLng,
-//           school.latitude,
-//           school.longitude
-//         ),
-//       }))
-//       .sort((a, b) => a.distance - b.distance);
+    const sortedSchools = schools
+      .map((school) => ({
+        ...school,
+        distance: calculateDistance(
+          userLat,
+          userLng,
+          school.latitude,
+          school.longitude
+        ),
+      }))
+      .sort((a, b) => a.distance - b.distance);
 
-//     res.json(sortedSchools);
-//   } catch (error) {
-//     res.status(500).json({ message: "Error fetching schools", error });
-//   }
-// });
+    res.json(sortedSchools);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching schools", error });
+  }
+});
 
 // //====================== Add School list api ====================================
 
-// app.post("/addSchool", async (req, res) => {
-//   try {
-//     const data = req.body;
-//     const validateRes = schema.validate(data);
-//     console.log(validateRes);
-//     if (validateRes.error) {
-//       res.status(400).send("Validation error");
-//       // console.log(data.error);
-//     } else {
-//       const sql =
-//         "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
-//       const values = [data.name, data.address, data.latitude, data.longitude];
+app.post("/addSchool", async (req, res) => {
+  try {
+    const data = req.body;
+    const validateRes = schema.validate(data);
+    console.log(validateRes);
+    if (validateRes.error) {
+      res.status(400).send("Validation error");
+      // console.log(data.error);
+    } else {
+      const sql =
+        "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)";
+      const values = [data.name, data.address, data.latitude, data.longitude];
 
-//       const result = await conn.query(sql, values);
-//       //const result = await conn.query("INSERT INTO schools SET?", data);
-//       res.status(201).send(result);
-//     }
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
+      const result = await conn.query(sql, values);
+      //const result = await conn.query("INSERT INTO schools SET?", data);
+      res.status(201).send(result);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
